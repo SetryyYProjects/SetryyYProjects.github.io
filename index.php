63cddb35d2cf4cef2e9a0d140243a6d7ff834836
SetryyYProjects <152201542+SetryyYProjects@users.noreply.github.com>
2024-01-13 18:54:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$PAGE__ = "test_page"
+?>
+<html>
+    <head>
+        <ttile>
+            <?php $PAGE__ ?>
+        </ttile>
+    </head>
+    <body>
+    <iframe src="https://discord.com/widget?id=1195744436945174558&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+    </body>
+</html>
